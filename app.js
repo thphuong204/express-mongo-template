ff1c26b5553bc9616fcf7fb1fb19ca476fa03055
thphuong204 <107916151+thphuong204@users.noreply.github.com>
2022-11-16 16:52:42 +0700

    remove require userRouter line, remove app.use(/users) line

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -21,6 +20,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors())
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 module.exports = app;
