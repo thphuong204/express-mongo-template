b4cf67f08e855140239caf4ea9d2bc0bfceecaca
thphuong204 <107916151+thphuong204@users.noreply.github.com>
2022-11-16 16:57:40 +0700

    Create a AppError class inherits from javascript Error class so that it could be throw

--- helpers/utils.js
+++ helpers/utils.js
@@ -7,3 +7,18 @@ utilsHelper.sendResponse = (res, status, success, data, errors, message) => {
     if (message) response.message = message;
     return res.status(status).json(response);
   };
+
+  class AppError extends Error {
+    constructor(statusCode, message, errorType) {
+      super(message);
+      this.statusCode = statusCode;
+      this.errorType = errorType;
+      // all errors using this class are operational errors.
+      this.isOperational = true;
+      // create a stack trace for debugging (Error obj, void obj to avoid stack polution)
+      Error.captureStackTrace(this, this.constructor);
+    }
+  }
+  
+  utilsHelper.AppError = AppError;
+  module.exports = utilsHelper;
